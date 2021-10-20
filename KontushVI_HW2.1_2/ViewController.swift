6bd6d07b556b83f5e462fee2c9e13c8aa3f1b9dc
Vadim <119fps.com>
2021-10-20 11:14:01 +0300

    Logic work

--- KontushVI_HW2.1_2/ViewController.swift
+++ KontushVI_HW2.1_2/ViewController.swift
@@ -9,11 +9,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var RedSquare: UIView!
+    @IBOutlet var YellowSquare: UIView!
+    @IBOutlet var GreenSquare: UIView!
+    @IBOutlet var ChangeColorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        RedSquare.alpha = 0.3
+        YellowSquare.alpha = 0.3
+        GreenSquare.alpha = 0.3
+        
+        RedSquare.layer.cornerRadius = 75
+        YellowSquare.layer.cornerRadius = 75
+        GreenSquare.layer.cornerRadius = 75
     }
 
-
+    var isRed = false
+    var isYellow = false
+    var isGreen = false
+    
+    @IBAction func ChangeColorButtonPressed() {
+        ChangeColorButton.setTitle("NEXT", for: .normal)
+        
+        
+        
+        if !isRed && !isYellow && !isGreen{
+            RedSquare.alpha = 1
+            isRed = true
+            return
+        }
+        
+        if isRed{
+            RedSquare.alpha = 0.3
+            YellowSquare.alpha = 1
+            GreenSquare.alpha = 0.3
+            
+            isRed = false
+            isYellow = true
+            return
+        }
+        
+        if isYellow{
+            RedSquare.alpha = 0.3
+            YellowSquare.alpha = 0.3
+            GreenSquare.alpha = 1
+            
+            isGreen = true
+            isYellow = false
+            return
+        }
+        
+        if isGreen{
+            RedSquare.alpha = 1
+            YellowSquare.alpha = 0.3
+            GreenSquare.alpha = 0.3
+            
+            isRed = true
+            isGreen = false
+            return
+        }
+        
+    }
+    
 }
 
